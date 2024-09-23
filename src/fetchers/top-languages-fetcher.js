27390d0b782648b80dbfdf1488861418acd8639a
Pedro Catalão <pedrocatalao@users.noreply.github.com>
2024-09-23 14:38:08 +0100

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -28,10 +28,10 @@ const fetcher = (variables, token) => {
       query userInfo($login: String!, $ownerAffiliations: [RepositoryAffiliation]) {
         user(login: $login) {
           # do not fetch forks
-          repositories(ownerAffiliations: $ownerAffiliations, isFork: false, first: 100) {
+          repositories(ownerAffiliations: $ownerAffiliations, isFork: false, first: 1) {
             nodes {
               name
-              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+              languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
                 edges {
                   size
                   node {
